7ce53085e7e82fd836ee6bfa5625c3e5093c14d7
Neeraj Gurung <neerajgurung2099@gmail.com>
2023-07-18 01:07:36 +0545

    UPdate on display result component

--- src/components/DisplayResult.tsx
+++ src/components/DisplayResult.tsx
@@ -1,10 +1,7 @@
 const DisplayResult = () => {
   return (
     <div className="w-full">
-      <input
-        placeholder="Enter"
-        className="w-full border-b-4 border-b-blue-400 text-right text-3xl"
-      />
+      <input className="w-full border-b-4 outline-0 border-b-blue-400 text-right text-3xl " />
     </div>
   );
 };
