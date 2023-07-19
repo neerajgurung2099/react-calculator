f7e8300cffa39cb5a2d3879157de2e1a13801f32
Neeraj Gurung <neerajgurung2099@gmail.com>
2023-07-19 23:49:46 +0545

    Made modal responsice

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -13,16 +13,16 @@ const Modal = ({ children }: Props) => {
     <div
       className={`${
         !modalVisible ? "hidden" : ""
-      } fixed w-full h-full left-0 top-0 z-10 bg-[rgba(0,0,0,0.4)]`}
+      } fixed w-full h-full left-0 top-0 z-10 bg-[rgba(0,0,0,0.4)] pt-24`}
     >
-      <div className="relative bg-[#fefefe] w-1/5 p-5 my-[15%] mx-auto rounded-sm">
+      <div className="relative bg-[#fefefe] w-4/5 lg:w-1/5 p-5 mx-auto rounded-sm  ">
         <span
           onClick={handleClick}
           className="absolute right-1.5 top-px text-2xl text-gray-400 cursor-pointer hover:text-black "
         >
           &times;
         </span>
-        <div className="flex flex-col gap-1">
+        <div className="flex flex-col gap-1 overflow-y-scroll h-[50vh]">
           {history.length !== 0 ? (
             history.map((item, index) => (
               <History
